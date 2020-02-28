4c2d32bc4b60049832fe93b244c5b704c292dd6e
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2020-02-28 07:06:02 +0100

    add function to get specnum from spec inputs

---
+++ estim/f_get_specnum.m
@@ -0,0 +1,36 @@
+function specnum = f_get_specnum(v, Nprior, Nr, Np, flag_survey, flag_recroll) 
+% this function returns the unique spec number relating to the given inputs
+% of spec variablesthat is used to call the compiled functions on the 
+% server. Useful for finding the spec number of a given spec that needs to
+% be executed locally. 
+
+% from PH_GER
+Nvintages = 1:157 ; 
+Nrs = 1:10 ; 
+Npriors = 1:5 ; 
+Nps = [1 3] ;
+Nmod = 1:4 ;
+
+% calculate combinations of specifications
+Nrs_temp = repmat(Nrs, 1, length(Npriors) * length(Nvintages) * length(Nps) * length(Nmod)) ; 
+Npriors_temp = repmat(kron(Npriors, ones(1, length(Nrs))), 1 , length(Nvintages) * length(Nps) * length(Nmod)) ;
+Nvintages_temp = repmat(kron(Nvintages, ones(1, length(Nrs) * length(Npriors))), 1, length(Nps) * length(Nmod)) ; 
+Nps_temp = repmat(kron(Nps, ones(1, length(Nrs) * length(Npriors) * length(Nvintages))), 1, length(Nmod)) ;
+Nmod_temp = kron(Nmod, ones(1, length(Nrs) * length(Npriors) * length(Nvintages) * length(Nps))) ;
+
+% map flags to Nmod
+if strcmp(flag_survey, 'level') && strcmp(flag_recroll, 'rec')
+    Nmod = 1;
+elseif strcmp(flag_survey, 'diff') && strcmp(flag_recroll, 'rec')
+    Nmod = 2 ; 
+elseif strcmp(flag_survey, 'level') && strcmp(flag_recroll, 'rolling')
+    Nmod = 3;
+elseif strcmp(flag_survey, 'diff') && strcmp(flag_recroll, 'rolling')
+    Nmod = 4 ; 
+end
+    
+% find spec number
+specnum = find( Nrs_temp == Nr & Nvintages_temp == v & Npriors_temp == Nprior & Nps_temp == Np & Nmod_temp == Nmod) ;
+
+
+
