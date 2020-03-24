8e4b5cf2cea5997379ea4619dd78f0e1a427a315
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2020-03-24 11:44:18 +0100

    turn f_eval_latex_table into function
    currently experimenting with non-standard values for subsamples,
    horizons and metrics to see how robust the code is. These lines need to
    be removed later on.

--- eval/f_eval_latex_table.m
+++ eval/f_eval_latex_table.m
@@ -1,127 +1,148 @@
-clear; close all; clc; 
-% flags 
-flag_survey = 'level';
-flag_sample = 'rec';
-flag_truegdp = 'first';
-Np = 1;
-flag_country = 'GER';
+function f_eval_latex_table(flag_survey, flag_sample, flag_truegdp, Np, flag_country)
 
-if strcmp(flag_country, 'GER')
-    name_country = 'Germany';
-elseif strcmp(flag_country, 'US')
-    name_country = 'United States';
-end
+    % - dir in & out ---------------------------------------------------- %
+    % ------------------------------------------------------------------- %
+    dir_load = ['C:\Users\Philipp\Documents\Dissertation\sparse nowcasting\eval\' flag_country '\' flag_survey ' ' flag_sample '\Np = ' num2str(Np) '\' flag_truegdp '\'] ;
+    dir_save = 'C:\Users\Philipp\Documents\Dissertation\sparse nowcasting\eval\latex_tables\' ; 
+    if exist(dir_save, 'dir') ~= 7;mkdir(dir_save); end  
 
-% dir in & out
-dir_load = ['C:\Users\Philipp\Documents\Dissertation\sparse nowcasting\eval\' flag_country '\' flag_survey ' ' flag_sample '\Np = ' num2str(Np) '\' flag_truegdp '\'] ;
-dir_save = 'C:\Users\Philipp\Documents\Dissertation\sparse nowcasting\eval\latex_tables\' ; 
-if exist(dir_save, 'dir') ~= 7;mkdir(dir_save); end  
+    % - load eval structure and options --------------------------------- %
+    % ------------------------------------------------------------------- %
+    load([dir_load 'results_eval.mat']);
+    evaloptions = load_evaloptions(flag_country);
 
-% font size
-fontsize = 'tiny'; 
+    % - USER INPUT ------------------------------------------------------ %
+    % ------------------------------------------------------------------- %
+    fontsize = 'tiny'; % 
+    linestep = '0.1'; % step size between model blocks
 
-% evaloptions (overwrite and amend)
-evaloptions = load_evaloptions(flag_country);
+    evaloptions.Nhs = [2, 4] ; % forecast horizons
+    evaloptions.Nhs = [1, 2, 4] ; % forecast horizons
 
-evaloptions.Nhs = [2, 4] ; 
+    % subsamples
+    evaloptions.indexstarts = evaloptions.indexstarts([1 3]);
+    evaloptions.indexends = evaloptions.indexends([1 3]);
+    evaloptions.names_subsamples = {'full sample', 'post-crisis sample'};
+    evaloptions.indexstarts = evaloptions.indexstarts([1]);
+    evaloptions.indexends = evaloptions.indexends([1]);
+    evaloptions.names_subsamples = {'full sample'};
+    
+    % forecast evaluation metrics
+    evaloptions.metrics = {'sfe', 'logscore', 'crps'}; 
+    evaloptions.names_metrics = {'RMSFE', 'logS', 'CRPS'}; 
+    evaloptions.metrics = {'sfe', 'logscore'}; 
+    evaloptions.names_metrics = {'RMSFE', 'logS'}; 
+    
+    % prior names
+    evaloptions.names_priors = {'NIG', 'MG', 'PMNM', 'HS', 'Nd'};
 
-evaloptions.indexstarts = evaloptions.indexstarts([1 3]);
-evaloptions.indexends = evaloptions.indexends([1 3]);
-evaloptions.names_subsamples = {'full sample', 'post-crisis sample'};
+    % label
+    str_label = ['table:' flag_country '_' flag_truegdp '_' flag_survey '_' flag_sample '_Np' num2str(Np)];
+    
+    % caption
+    if strcmp(flag_country, 'GER'); name_country = 'Germany'; elseif strcmp(flag_country, 'US'); name_country = 'United States'; end
+    str_caption = [name_country ' (' flag_truegdp ', ' flag_survey ', ' flag_sample ', P = ' num2str(Np) ')']; 
 
-evaloptions.metrics = {'sfe', 'logscore', 'crps'}; 
-evaloptions.names_metrics = {'RMSFE', 'logS', 'CRPS'}; 
+    % notes to table
+    str_notes = ['RMSFE, logS and CRPS for the models are relativ to the B-AR benchmark (see text for details).' ...
+                 'The log score is negatively orientated so that a value in the table below 1 corresponds to a better performance than the benchmark.' ...
+                 'Forecast horizon h is in months. The full sample period is 2000Q1-2018Q4, the post-crisis sample starts in 2010Q1 and ends in 2018Q4.'];
 
-evaloptions.names_priors = {'NIG', 'MG', 'PMNM', 'HS', 'Nd'};
+    % - open file ------------------------------------------------------- %
+    % ------------------------------------------------------------------- %
+    filename = ['table_' flag_country '_' flag_truegdp '_' flag_survey '_' flag_sample '_Np' num2str(Np) '.tex'];
+    fid = fopen([dir_save filename], 'w'); 
 
-% load eval structure
-load([dir_load 'results_eval.mat'])
+    % - upper body ------------------------------------------------------ %
+    % ------------------------------------------------------------------- %
+    
+    Ncols_per_sample = length(evaloptions.names_subsamples) * length(evaloptions.Nhs) * length(evaloptions.metrics);
 
+    fprintf(fid, ['\\begin{threeparttable}[p]\n\\caption{' str_caption '}\n\\label{' str_label '}\n\\' fontsize '\n']);
+    fprintf(fid, '\\begin{tabular}{ c l');
 
-% table info
-Ncol_prior = 1; 
-Ncol_model_benchmark = 1;
-Ncols_per_sample = length(evaloptions.names_subsamples) * length(evaloptions.Nhs) * length(evaloptions.metrics);
+    tmp1 = repmat(' c ', 1, length(evaloptions.Nhs));
+    tmp2 = [repmat([tmp1 ' | '], 1, length(evaloptions.metrics) - 1) tmp1];
+    counter = 1;
+    tmp3 = tmp2;
+    while counter < length(evaloptions.names_subsamples)
+        counter = counter + 1;
+        tmp3 = [tmp3 ' !{\\vrule width 0.7pt} ' tmp2];
+    end
+    fprintf(fid,[tmp3 '}\n\\toprule\n']);
 
+    counter = 1;
+    str_tmp = ['\\multicolumn{' num2str(Ncols_per_sample/length(evaloptions.names_subsamples)) '}{c}{' evaloptions.names_subsamples{counter} '}'];
+    while counter < length(evaloptions.names_subsamples)
+        counter = counter + 1;
+        str_tmp = [str_tmp [' & \\multicolumn{' num2str(Ncols_per_sample/length(evaloptions.names_subsamples)) '}{c}{' evaloptions.names_subsamples{counter} '}']];
+    end
+    fprintf(fid, [' & & ' str_tmp '\\\\\n']);
 
-% open file
-filename = ['table_' flag_country '_' flag_truegdp '_' flag_survey '_' flag_sample '_Np' num2str(Np) '.tex'];
-fid = fopen([dir_save filename], 'w'); 
+    counter = 1;
+    str_tmp = ['\\multicolumn{' num2str(length(evaloptions.Nhs)) '}{c}{' evaloptions.names_metrics{counter} '}'];
+    while counter < length(evaloptions.names_metrics)
+        counter = counter + 1;
+        str_tmp = [str_tmp [' & \\multicolumn{' num2str(length(evaloptions.Nhs)) '}{c}{' evaloptions.names_metrics{counter} '}']];
+    end
+    fprintf(fid, [' & & ' [repmat(str_tmp, 1, length(evaloptions.names_subsamples)-1) ' & ' str_tmp] '\\\\\n']);
 
-% upper body
-str_label = 'label1';
-str_caption = 'captionA';
-fprintf(fid, ['\\begin{threeparttable}[p]\n\\caption{' str_caption '}\n\\label{' str_label '}\n\\' fontsize '\n']);
-fprintf(fid, '\\begin{tabular}{ c l ');
-fprintf(fid, [repmat('c ', 1, Ncols_per_sample) '}\n\\toprule\n']);
+    counter = 1;
+    str_tmp = results_eval.priors(1).R(1).horizon(evaloptions.Nhs(counter)).name;
+    while counter < length(evaloptions.Nhs)
+        counter = counter + 1;
+        str_tmp = [str_tmp [' & ' results_eval.priors(1).R(1).horizon(evaloptions.Nhs(counter)).name]];
+    end
+    fprintf(fid, [' & & ' repmat([str_tmp ' & '], 1, length(evaloptions.metrics) * length(evaloptions.names_subsamples)-1) str_tmp '\\\\\n']);
 
-counter = 1;
-str_tmp = ['\\multicolumn{' num2str(Ncols_per_sample/length(evaloptions.names_subsamples)) '}{c}{' evaloptions.names_subsamples{counter} '}'];
-while counter < length(evaloptions.names_subsamples)
-    counter = counter + 1;
-    str_tmp = [str_tmp [' & \\multicolumn{' num2str(Ncols_per_sample/length(evaloptions.names_subsamples)) '}{c}{' evaloptions.names_subsamples{counter} '}']];
-end
-fprintf(fid, [' & & ' str_tmp '\\\\\n']);
+    fprintf(fid, '\\midrule\n');
+    
+    % - benchmark ------------------------------------------------------- %
+    % ------------------------------------------------------------------- %
+    
+    vals_bar = getvals(results_eval, [], [], 0, 1, evaloptions);
+    addline(fid, vals_bar, 'B-AR', [])
+    fprintf(fid, ['\\hspace{' linestep 'cm}\\\\\n']);
 
-counter = 1;
-str_tmp = ['\\multicolumn{' num2str(length(evaloptions.Nhs)) '}{c}{' evaloptions.names_metrics{counter} '}'];
-while counter < length(evaloptions.names_metrics)
-    counter = counter + 1;
-    str_tmp = [str_tmp [' & \\multicolumn{' num2str(length(evaloptions.Nhs)) '}{c}{' evaloptions.names_metrics{counter} '}']];
-end
-fprintf(fid, [' & & ' [repmat(str_tmp, 1, length(evaloptions.names_subsamples)-1) ' & ' str_tmp] '\\\\\n']);
+    % - models for different R ------------------------------------------ %
+    % ------------------------------------------------------------------- %
+    for r = evaloptions.Nrs
+        for p = evaloptions.Npriorspecs
+            vals = getvals(results_eval, p, r, 0, 0, evaloptions);
+            if p == evaloptions.Npriorspecs(1) 
+                % print R=r in first column      
+                addline(fid, vals ./ vals_bar, ['R=' num2str(r)], evaloptions.names_priors{p})
+            else
+                addline(fid, vals ./ vals_bar, '', evaloptions.names_priors{p})
+            end
+        end
+        fprintf(fid, ['\\hspace{' linestep 'cm}\\\\\n']);
+    end
 
-counter = 1;
-str_tmp = results_eval.priors(1).R(1).horizon(evaloptions.Nhs(counter)).name;
-while counter < length(evaloptions.Nhs)
-    counter = counter + 1;
-    str_tmp = [str_tmp [' & ' results_eval.priors(1).R(1).horizon(evaloptions.Nhs(counter)).name]];
-end
-fprintf(fid, [' & & ' repmat([str_tmp ' & '], 1, length(evaloptions.metrics) * length(evaloptions.names_subsamples)-1) str_tmp '\\\\\n']);
-
-fprintf(fid, '\\midrule\n');
-
-% benchmark
-vals_bar = getvals(results_eval, [], [], 0, 1, evaloptions);
-addline(fid, vals_bar, 'B-AR', [])
-fprintf(fid, '\\hspace{0.1cm}\\\\\n');
-
-% loop over R's
-for r = evaloptions.Nrs
+    % - pool (only loop over priors!) ----------------------------------- %
+    % ------------------------------------------------------------------- %
     for p = evaloptions.Npriorspecs
-        vals = getvals(results_eval, p, r, 0, 0, evaloptions);
+        vals = getvals(results_eval, p, [], 1, 0, evaloptions);
         if p == evaloptions.Npriorspecs(1) 
             % print R=r in first column      
-            addline(fid, vals ./ vals_bar, ['R=' num2str(r)], evaloptions.names_priors{p})
+            addline(fid, vals ./ vals_bar, 'pool', evaloptions.names_priors{p})
         else
             addline(fid, vals ./ vals_bar, '', evaloptions.names_priors{p})
         end
     end
-    fprintf(fid, '\\hspace{0.1cm}\\\\\n');
+
+    % - append lower body ----------------------------------------------- %
+    % ------------------------------------------------------------------- %
+    fprintf(fid, ['\\bottomrule\n\\end{tabular}\n\\begin{tablenotes}\n\\' fontsize '\n\\item ' str_notes]);
+    fprintf(fid, '\n\\end{tablenotes}\n\\end{threeparttable}\n');
+
+    % - close file ------------------------------------------------------ %
+    % ------------------------------------------------------------------- %
+    fclose(fid);
 end
 
-% pool (only loop over priors!)
-for p = evaloptions.Npriorspecs
-    vals = getvals(results_eval, p, [], 1, 0, evaloptions);
-    if p == evaloptions.Npriorspecs(1) 
-        % print R=r in first column      
-        addline(fid, vals ./ vals_bar, 'pool', evaloptions.names_priors{p})
-    else
-        addline(fid, vals ./ vals_bar, '', evaloptions.names_priors{p})
-    end
-end
-
-% append lower body
-fprintf(fid, '\\bottomrule\n\\end{tabular}\n\\begin{tablenotes}\n\\' fontsize '\n\\item ');
-
-% notes under tables (split across multiple fprintf's for convenience
-fprintf(fid, 'RMSFE, logS and CRPS for the models are relativ to the B-AR benchmark (see text for details)');
-fprintf(fid, 'The log score is negatively orientated so that a value in the table below 1 corresponds to a better performance than the benchmark.');
-fprintf(fid, 'Forecast horizon h is in months. The full sample period is 2000Q1-2018Q4, the post-crisis sample starts in 2010Q1 and ends in 2018Q4.'); 
-fprintf(fid, '\n\\end{tablenotes}\n\\end{threeparttable}\n');
-
-% close file
-fclose(fid);
+% - FUNCTIONS ----------------------------------------------------------- %
+% ----------------------------------------------------------------------- %
 
 function vals = getvals(results_eval, p, r, flag_pool, flag_benchmark, evaloptions)
 % function that loops over samples, forecast metrics and horizons and
@@ -152,6 +173,11 @@ function vals = getvals(results_eval, p, r, flag_pool, flag_benchmark, evaloptio
 end
 
 function addline(fid, vals, r_name, p_name)
+
+% r_name in bold!
+if not(isempty(r_name))
+    r_name = ['\textbf{' r_name '}'];
+end
 
 % add leading columns and values
 fprintf(fid, ['%s & %s & %.2f' repmat(' & %.2f ', 1, length(vals)-1), ' %.2f'], r_name, p_name, vals);
