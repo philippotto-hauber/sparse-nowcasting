31bffeaf62ee0138e66308515d4a26b5e1c1e588
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2020-05-11 19:07:04 +0200

    calc ineff fac in par but on my machine

--- eval/calc_inefffac.R
+++ eval/calc_inefffac.R
@@ -6,10 +6,10 @@ library(coda)
 library(rmatio)
 library(ggplot2)
 library(dplyr)
-library(parallel)
+library(doParallel)
 
 # parallel
-registerDoParallel(cores=20)
+registerDoParallel(cores=4)
 getDoParWorkers()
 
 # number of draws
@@ -45,14 +45,14 @@ for (country in Ncountries)
   if (country == "GER")
   {
     Nvintages <- seq(1, 157)
-    Nvintages <- seq(1, 10)
-    dirname <- "../PH_GER/"
+    dirname <- "C:/Users/Philipp/Documents/Dissertation/sparse nowcasting/estim/PH_GER/"
+    #dirname <- "../PH_GER/"
   } 
   else if (country == "US")
   {
     Nvintages <- seq(1, 229) 
-    Nvintages <- seq(1, 10)
-    dirname <- "../PH_US/"
+    dirname <- "C:/Users/Philipp/Documents/Dissertation/sparse nowcasting/estim/PH_US/"
+    #dirname <- "../PH_US/"
   }
   
   # merge spec details into one big loop
@@ -130,24 +130,5 @@ for (country in Ncountries)
   ineff_facs <- rbind(ineff_facs, tmp_df)
 }
 
-# plots
-library(forcats)
-ggplot(ineff_facs) + 
-  geom_boxplot(aes(x=fct_rev(prior),y=value))+
-  facet_wrap( ~ country, scales = "free_y")+
-  labs(x = "", y = "inefficiency factor")+
-  coord_flip()
-
-# alternative plots
-ggplot(ineff_facs) + 
-  geom_boxplot(aes(x=prior,y=value, color = country), outlier.size=1)+
-  labs(x = "", y = "", caption = "Blablabla")+
-  facet_grid(surveysample ~ Np, scales = "free_y")+
-  theme_minimal()+
-  theme(strip.text.x = element_text(size = 8),
-        strip.text.y = element_text(size = 8),
-        legend.title = element_blank())
-
-# save file as full size pdf
-#ggsave(file="a4_output.pdf", width = 0.5*210, height = 0.5*297, units = "mm")
-ggsave(file="plot_ineff_facs.pdf", width = 150, height = 150, units = "mm")
+# save to file
+save(ineff_facs, file = "C:/Users/Philipp/Documents/Dissertation/sparse nowcasting/estim/ineff_facs.Rda")
