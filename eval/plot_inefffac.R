9cd8aa7ffeef60a064b1a9c7b33b7d140ce35c3f
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2020-05-12 08:55:04 +0200

    rm vert grid lines, update paths

--- eval/plot_inefffac.R
+++ eval/plot_inefffac.R
@@ -1,8 +1,14 @@
 rm(list=ls())
-load("C:/Users/Philipp/Documents/Dissertation/sparse nowcasting/estim/ineff_facs.Rda")
+library(forcats)
+library(ggplot2)
+dirname <- "C:/Users/Philipp/Documents/Dissertation/sparse nowcasting/estim/"
+filename_in <- "ineff_facs.Rda"
+filename_out <- "plot_ineff_facs.pdf"
+
+# load data
+load(paste0(dirname, filename_in))
 
 # plots
-library(forcats)
 ggplot(ineff_facs) + 
   geom_boxplot(aes(x=fct_rev(prior),y=value))+
   facet_wrap( ~ country, scales = "free_y")+
@@ -11,14 +17,16 @@ ggplot(ineff_facs) +
 
 # alternative plots
 ggplot(ineff_facs) + 
-  geom_boxplot(aes(x=prior,y=value, color = country), outlier.size=1)+
+  geom_boxplot(aes(x=prior,y=value, color = country), outlier.size = 1, outlier.shape = 16)+
   labs(x = "", y = "", caption = "Blablabla")+
-  facet_grid(surveysample ~ Np, scales = "free_y")+
+  facet_grid(surveysample ~ Np)+
+  scale_y_continuous(breaks = c(50, 100), minor_breaks = c(50, 100))+
   theme_minimal()+
   theme(strip.text.x = element_text(size = 8),
         strip.text.y = element_text(size = 8),
-        legend.title = element_blank())
+        legend.title = element_blank(),
+        panel.grid.major.x = element_blank())
 
 # save file as full size pdf
 #ggsave(file="a4_output.pdf", width = 0.5*210, height = 0.5*297, units = "mm")
-ggsave(file="plot_ineff_facs.pdf", width = 150, height = 150, units = "mm")
+ggsave(file = paste0(dirname, filename_out), width = 150, height = 150, units = "mm")
