bcf31df7e409e4adb8e7ab8134b9eb5ab4bb96d1
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2020-05-26 12:50:46 +0200

    update caption and legend position

--- estim/plot_inefffac.R
+++ estim/plot_inefffac.R
@@ -18,14 +18,16 @@ ggplot(ineff_facs) +
 # alternative plots
 ggplot(ineff_facs) + 
   geom_boxplot(aes(x=prior,y=value, color = country), outlier.size = 1, outlier.shape = 16)+
-  labs(x = "", y = "", caption = "Blablabla")+
+  labs(x = "", y = "", caption = "P: number of lags in factor VAR, rec/rolling: recursive or rolling estimation window, level/diff: survey variables in levels or first difference\nPriors: HS+ = Horseshoe plus, MG = multiplicative Gamma, NIG = Normal Inverse Gamma, Nd = Normal diffuse, PMNM = point mass normal mixture.\n For details, see main text.")+
   facet_grid(surveysample ~ Np)+
   scale_y_continuous(breaks = c(50, 100), minor_breaks = c(50, 100))+
   theme_minimal()+
   theme(strip.text.x = element_text(size = 8),
         strip.text.y = element_text(size = 8),
         legend.title = element_blank(),
-        panel.grid.major.x = element_blank())
+        legend.position = "bottom",
+        panel.grid.major.x = element_blank(),
+        plot.caption = element_text(size = 5))
 
 # save file as full size pdf
 #ggsave(file="a4_output.pdf", width = 0.5*210, height = 0.5*297, units = "mm")
