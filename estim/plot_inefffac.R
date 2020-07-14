871a7cae0c2bf381ff02dcf92914a38bb05443da
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2020-07-14 08:21:03 +0200

    update plot of ineff facs

--- estim/plot_inefffac.R
+++ estim/plot_inefffac.R
@@ -1,6 +1,7 @@
 rm(list=ls())
 library(forcats)
 library(ggplot2)
+library(dplyr)
 dirname <- "C:/Users/Philipp/Documents/Dissertation/sparse nowcasting/estim/"
 filename_in <- "ineff_facs.Rda"
 filename_out <- "plot_ineff_facs.pdf"
@@ -8,18 +9,18 @@ filename_out <- "plot_ineff_facs.pdf"
 # load data
 load(paste0(dirname, filename_in))
 
-# filter out Np == 1
+# filter Np == 1
 ineff_facs %>% filter(Np == "P = 1") -> ineff_facs
 
 # plots
 ggplot(ineff_facs) + 
   geom_boxplot(aes(x=prior,y=value, color = country), outlier.size = 1, outlier.shape = 16)+
-  labs(x = "", y = "", caption = "rec/rolling: recursive or rolling estimation window, level/diff: survey variables in levels or first difference\nPriors: HS+ = Horseshoe plus, MG = multiplicative Gamma, NIG = Normal Inverse Gamma, \nNd = Normal diffuse, PMNM = point mass normal mixture. For details, see main text.")+
-  facet_grid(surveysample ~ Np)+
+  labs(x = "", y = "", caption = "rec/rolling: recursive or rolling estimation window, level/diff: survey variables in levels or first differences\nPriors: HS+ = Horseshoe plus, MG = multiplicative Gamma, NIG = Normal Inverse Gamma, \nNd = Normal diffuse, PMNM = point mass normal mixture. For details, see main text.")+
+  facet_grid(sample ~ survey)+
   scale_y_continuous(breaks = c(50, 100), minor_breaks = c(50, 100))+
   theme_minimal()+
-  theme(strip.text.x = element_text(size = 8),
-        strip.text.y = element_text(size = 8),
+  theme(strip.text.x = element_text(size = 10),
+        strip.text.y = element_text(size = 10),
         legend.title = element_blank(),
         legend.position = "bottom",
         panel.grid.major.x = element_blank(),
@@ -27,4 +28,4 @@ ggplot(ineff_facs) +
 
 # save file as full size pdf
 #ggsave(file="a4_output.pdf", width = 0.5*210, height = 0.5*297, units = "mm")
-#ggsave(file = paste0(dirname, filename_out), width = 150, height = 150, units = "mm")
+ggsave(file = paste0(dirname, filename_out), width = 150, height = 150, units = "mm")
