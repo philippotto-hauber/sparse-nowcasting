9c80aaae04b025bdaebc2f84a7ac9172fb509c2e
Philipp Hauber <philippottohauber@gmail.com>
2021-05-11 21:04:36 +0200

    fix shell script

--- eval/compile_batch_eval_hpc.sh
+++ eval/compile_batch_eval_hpc.sh
@@ -2,5 +2,5 @@
 module load matlab/2020b
 mcc -m batch_eval_hpc.m
 
-mv batch_eval_hpc ./eval-hpc/
-rm mccExcludedFiles.log requiredMCRProducts.txt readme.txt batch_eval_hpc.sh
+mv batch_eval_hpc ./../../eval-hpc
+rm mccExcludedFiles.log requiredMCRProducts.txt readme.txt run_batch_eval_hpc.sh
