8a065094731b8f1c858312b3ba18b38c767fc25e
Philipp Hauber <philippottohauber@gmail.com>
2021-05-14 17:02:08 +0200

    reset dir_truegdp

--- eval/f_eval_plot_pointnowcasts.m
+++ eval/f_eval_plot_pointnowcasts.m
@@ -9,7 +9,6 @@ function f_eval_plot_pointnowcasts(flag_survey, flag_sample, flag_truegdp, Np, f
     dir_save = [pwd '/' flag_country '/' flag_survey ' ' flag_sample '/Np = ' num2str(Np) '/' flag_truegdp '/graphs/'] ; 
     if exist(dir_save, 'dir') ~= 7;mkdir(dir_save); end  
     dir_truegdp = '' ;
-    dir_truegdp = 'C:\Users\Philipp\Documents\Dissertation\sparse nowcasting\data\out\';
 
     % - load forecast structure
     % -----------------
