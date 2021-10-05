d13169159e7ec170964b798befeb644624ea7b64
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2021-10-05 07:51:05 +0200

    increase size of dots, rm caption

--- estim/plot_inefffac.R
+++ estim/plot_inefffac.R
@@ -14,15 +14,16 @@ ineff_facs %>% filter(Np == "P = 1") -> ineff_facs
 
 # plots
 ggplot(ineff_facs) + 
-  geom_boxplot(aes(x=prior,y=value, color = country), outlier.size = 1, outlier.shape = 16)+
-  labs(x = "", y = "", caption = "rec/rolling: recursive or rolling estimation window, level/diff: survey variables in levels or first differences\nPriors: HS+ = Horseshoe plus, MG = multiplicative Gamma, NIG = Normal Inverse Gamma, \nNd = Normal diffuse, PMNM = point mass normal mixture. For details, see main text.")+
+  geom_boxplot(aes(x=prior,y=value, color = country), outlier.size = 2, outlier.shape = 16)+
+  #labs(x = "", y = "", caption = "rec/rolling: recursive or rolling estimation window, level/diff: survey variables in levels or first differences\nPriors: HS+ = Horseshoe plus, MG = multiplicative Gamma, NIG = Normal Inverse Gamma, \nNd = Normal diffuse, PMNM = point mass normal mixture. For details, see main text.")+
+  labs(x = "", y = "", caption = "") +
   facet_grid(sample ~ survey)+
   scale_y_continuous(breaks = c(50, 100), minor_breaks = c(50, 100))+
   theme_minimal()+
   theme(strip.text.x = element_text(size = 10),
         strip.text.y = element_text(size = 10),
         legend.title = element_blank(),
-        legend.position = "bottom",
+        legend.position = "top",
         panel.grid.major.x = element_blank(),
         plot.caption = element_text(size = 8))
 
